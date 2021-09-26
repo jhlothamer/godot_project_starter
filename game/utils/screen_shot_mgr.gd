075e71ffd126f7a56dc5ab12cf4b65ca80331b97
Jason Lothamer <jhlothamer@gmail.com>
2021-09-26 17:50:26 -0400

    refactor screenshot mgr, improve screenshot performance with threads, introduce screenshot project settings

--- game/utils/screen_shot_mgr.gd
+++ game/utils/screen_shot_mgr.gd
@@ -1,57 +1,76 @@
 extends Node
 
-export (int, 1, 100000) var screenshot_max : int = 1000
-export (float, 0.0, 2.0) var resize_factor : float = 1.0
-export var enabled : bool
-export var screenshot_action_name : String = "screen_shot"
+var _screenshot_max : int = 1000
+var _resize_factor : float = 1.0
+var _screenshot_action_name : String = "toggle_screenshots"
+var _directory_name := ""
+var _file_counter := 0
+var _user_data_directory : Directory = Directory.new()
+var _threads := []
 
-var allow_input : bool = true
-var take_screenshots : bool = false
-var directory_name : String = ""
-var file_counter : int = 0
-var user_data_directory : Directory = Directory.new()
 
 func _ready():
-	user_data_directory.open("user://")
+	if !OS.has_feature("debug"):
+		queue_free()
+		return
+	set_process(false)
+	_user_data_directory.open("user://")
+	_init_settings()
+
+func _init_settings():
+	var temp = ProjectSettings.get_setting("global/screenshot_mgr_max_images")
+	if temp and temp > 0:
+		_screenshot_max = temp
+	
+	temp = ProjectSettings.get_setting("global/screenshot_mgr_resize_factor")
+	if temp and temp > 0.0:
+		_resize_factor = temp
+
+	temp = ProjectSettings.get_setting("global/screenshot_mgr_toggle_action_name")
+	if temp:
+		_screenshot_action_name = temp
+
 
 func _input(event):
-	if !allow_input || !Input.is_action_just_pressed(screenshot_action_name):
+	if !event.is_action_pressed(_screenshot_action_name) or event.is_echo():
 		return
-	allow_input = false
-	$inputDebounceTimer.start()
-	take_screenshots = !take_screenshots
-	if take_screenshots:
-		directory_name = get_date_time_string()
-		file_counter = 0
-		user_data_directory.make_dir(directory_name)
+	if !is_processing():
+		_directory_name = get_date_time_string()
+		_file_counter = 0
+		_user_data_directory.make_dir(_directory_name)
 		print("starting screenshots")
+		set_process(true)
 	else:
-		print("stopping screenshots with " + String(file_counter) + " shots taken.")
+		print("stopping screenshots with %d shots taken." % _file_counter)
+		set_process(false)
 
-func _on_inputDebounceTimer_timeout():
-	allow_input = true
 
 func get_date_time_string():
 	# year, month, day, weekday, dst (daylight savings time), hour, minute, second.
 	var datetime = OS.get_datetime()
-	return String(datetime["year"]) + i_to_padded(datetime["month"],2) + i_to_padded(datetime["day"],2) + "_" + i_to_padded(datetime["hour"],2) + i_to_padded(datetime["minute"],2) + i_to_padded(datetime["second"],2)
-
-func i_to_padded(i : int, digits: int) -> String:
-	return String(i).pad_zeros(digits)
+	return "%d%02d%02d_%02d%02d%02d" % [datetime["year"], datetime["month"], datetime["day"], datetime["hour"], datetime["minute"], datetime["second"]]
 
 
 func _process(delta):
-	if !take_screenshots:
-		return
-	if file_counter >= screenshot_max:
-		take_screenshots = false
-		print("Max screenshots reached.  Stopping screenshots with " + String(file_counter) + " shots taken.")	
+	if _file_counter >= _screenshot_max:
+		print("Max screenshots reached.  Stopping screenshots with %d shots taken." % _file_counter)
+		set_process(false)
 		return
 
 	var image = get_viewport().get_texture().get_data()
+	var thread := Thread.new()
+	_threads.append(thread)
+	thread.start(self, "_save_image", [image, thread])
+	_file_counter += 1
+
+
+func _save_image(data: Array) -> void:
+	var image: Image = data[0]
 	image.flip_y()
-	if resize_factor != 1.0:
+	if _resize_factor != 1.0:
 		var original_size = image.get_size()
-		image.resize(original_size.x*resize_factor, original_size.y*resize_factor, Image.INTERPOLATE_BILINEAR)
-	image.save_png("user://" + directory_name + "/" + i_to_padded(file_counter,4) + ".png")
-	file_counter += 1
+		image.resize(original_size.x*_resize_factor, original_size.y*_resize_factor, Image.INTERPOLATE_BILINEAR)
+	var error = image.save_png("user://%s/%04d.png" % [_directory_name, _file_counter])
+	if error != OK:
+		printerr("ScreenshotMgr: error while saving image %d" % error)
+	_threads.erase(data[1])
