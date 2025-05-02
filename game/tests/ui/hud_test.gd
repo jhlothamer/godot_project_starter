2da7c0e376fc1573e5170e0104077b555dc3aa7f
Jason Lothamer <jhlothamer@gmail.com>
2025-05-02 06:06:43 -0400

    switch to using event bus - first use is game over dialog

--- game/tests/ui/hud_test.gd
+++ game/tests/ui/hud_test.gd
@@ -2,7 +2,4 @@ extends Control
 
 
 func _on_SimulateGameOverBtn_pressed():
-	var a = InputEventAction.new()
-	a.action = "game_over"
-	a.pressed = true
-	Input.parse_input_event(a)
+	EventBus.game_over.emit()
